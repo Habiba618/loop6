f6eb5379f13f6b1310350e2a214e73de59be12e0
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-15 09:37:38 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         Java ile 1 - 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.
+               for (int sayi = 2; sayi <= 100; sayi++) {
+         boolean asal = true;
+                for (int bolen = 2; bolen < sayi; bolen++) {
+                    if (sayi % bolen == 0) {
+                             asal = false;
+                        break;
+                    }
+                }
+                if (asal) {
+                    System.out.print(sayi + " ");
+                }
+            } */
+        //Java döngüler ile fibonacci serisi bulan program
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ededi daxil edin: ");
+        int a = sc.nextInt();
+        int first = 0;
+        int second=1;
+        int third;
+        System.out.print(first+" "+second);
+        for(int i =0; i<=a-2; i++){
+            third=first+second;
+            first=second;
+            second=third;
+            System.out.print(" "+third);
+        }
     }
 }
